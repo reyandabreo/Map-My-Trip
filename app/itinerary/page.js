5fc5548bc492ef4585d44b12333475b2cfc3fb40
safinalphanso <safin.alphanso@gmail.com>
2025-04-04 00:58:06 +0530

    feature changes

--- app/itinerary/page.js
+++ app/itinerary/page.js
@@ -495,7 +495,7 @@ const ItineraryComponent = () => {
                               {isCurrent && (
                                 <div 
                                   ref={personRef}
-                                  className="absolute -right-8 transform transition-transform duration-500"
+                                  className="absolute -right-12 transform transition-transform duration-500 z-10"
                                 >
                                   <motion.div 
                                     className={`${currentTheme.secondaryColor} p-2 rounded-full shadow-lg`}
@@ -633,6 +633,18 @@ const ItineraryComponent = () => {
                                       </button>
                                     </div>
                                   </div>
+                                  
+                                  {/* Price information */}
+                                  {activity.price && (
+                                    <div className="mt-2 p-2 bg-green-50 rounded-lg text-green-700 text-sm font-medium">
+                                      <span className="flex items-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        </svg>
+                                        Estimated Cost: {activity.price}
+                                      </span>
+                                    </div>
+                                  )}
                                   
                                   {activity.notes && (
                                     <div className="mt-3 p-3 bg-blue-50 rounded-lg text-gray-700 text-sm">
